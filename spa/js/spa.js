acf120395647702084190fbddd7b59a608ff9fd8
zinglax <zinglax@github.com>
2016-12-26 16:12:02 -0500

    Chapter 3.4.4 pg 78

--- spa/js/spa.js
+++ spa/js/spa.js
@@ -15,11 +15,7 @@ white:true
 
 var spa = (function () {
     var initModule = function ( $container ) {
-        $container.html(
-            '<h1 style="display:inline-block; margin:25px;">'
-            + 'hello world!'
-            + '</h1>'
-        );
+        spa.shell.initModule( $container );
     };
 
     return { initModule: initModule };
--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -14,49 +14,48 @@ white:true
 
 /*global $, spa */
 
-spa.shell = (function () {
-	//---------- BEGIN MODULE SCOPE VARIABLES ----------
-	var configMap = {
-		main_html : String()
-			+ 	'<div class="spa-shell-head">'+
-					'<div class="spa-shell-head-logo"></div>'+
-					'<div class="spa-shell-head-acct"></div>'+
-					'<div class="spa-shell-head-search"></div>'+
-				'</div>'+
-				'<div class="spa-shell-main ">'+
-					'<div class="spa-shell-main-nav"></div>'+
-					'<div class="spa-shell-main-content"></div>'+
-				'</div>'+
-				'<div class="spa-shell-foot"></div>'+
-				'<div class="spa-shell-chat"></div>'+
-				'<div class="spa-shell-modal"></div>';
-	},
-	stateMap = { $container : null },
-	jqueryMap = {},
+spa.shell = (function() {
+    //---------- BEGIN MODULE SCOPE VARIABLES ----------
+    var configMap = {
+            main_html: String() + '<div class="spa-shell-head">' +
+                '<div class="spa-shell-head-logo"></div>' +
+                '<div class="spa-shell-head-acct"></div>' +
+                '<div class="spa-shell-head-search"></div>' +
+                '</div>' +
+                '<div class="spa-shell-main ">' +
+                '<div class="spa-shell-main-nav"></div>' +
+                '<div class="spa-shell-main-content"></div>' +
+                '</div>' +
+                '<div class="spa-shell-foot"></div>' +
+                '<div class="spa-shell-chat"></div>' +
+                '<div class="spa-shell-modal"></div>'
+        },
+        stateMap = { $container: null },
+        jqueryMap = {},
 
-	setJqueryMap, initModule;
-	//---------- END MODULE SCOPE VARIABLES ----------
+        setJqueryMap, initModule;
+    //---------- END MODULE SCOPE VARIABLES ----------
 
-	//---------- BEGIN UTILITY METHODS ----------
-	//---------- END UTILITY METHODS ----------
+    //---------- BEGIN UTILITY METHODS ----------
+    //---------- END UTILITY METHODS ----------
 
-	//---------- BEGIN DOM METHODS ----------
-	setJqueryMap = function () {
-		var $container = stateMap.$container;
-		jqueryMap = { $container : $container };
+    //---------- BEGIN DOM METHODS ----------
+    setJqueryMap = function() {
+        var $container = stateMap.$container;
+        jqueryMap = { $container: $container };
+    };
+    //---------- END DOM METHODS ----------
+
+    //---------- BEGIN EVENT HANDLERS ----------
+    //---------- END EVENT HANDLERS ----------
+
+    //---------- BEGIN PUBLIC METHODS ----------
+    initModule = function($container) {
+        stateMap.$container = $container;
+        $container.html(configMap.main_html);
+        setJqueryMap();
 	};
-	//---------- END DOM METHODS ----------
+    //---------- END PUBLIC METHODS ----------
 
-	//---------- BEGIN EVENT HANDLERS ----------
-	//---------- END EVENT HANDLERS ----------
-
-	//---------- BEGIN PUBLIC METHODS ----------
-	initModule = function ( $container ) {
-		stateMap.$container = $container;
-		$container.html( configMap.main_html );
-		setJqueryMap();
-	}
-	//---------- END PUBLIC METHODS ----------
-
-	return { initModule : initModule };
+    return { initModule: initModule };
 }());
